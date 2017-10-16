6e9d4fbc4baf1fd852f85d7058c7baa31876c273
Damien Savatier <damien.savatier@epitech.eu>
2017-10-17 00:54:22 +0200

    Use wal instead of feh

--- i3+polybar/.config/bgloop.sh
+++ i3+polybar/.config/bgloop.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-folder="${HOME}"'/Pictures/Wallpapers/chainsaw/'
+folder="${HOME}"'/Pictures/Wallpapers/Space/'
 pause=100
 while true; do
     pic=$(find "$folder"* | shuf -n 1)
-    feh --no-fehbg --bg-fill "$pic"
+#    feh --no-fehbg --bg-fill "$pic"
+    wal -i "$pic"
     sleep $pause
 done
 # vim:ft=sh
