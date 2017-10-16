d378485ed08dac00382d77e28dd179189036e29f
Damien Savatier <damien.savatier@epitech.eu>
2017-10-17 00:55:08 +0200

    Better oneko script, reduced infinite loops and dead processes (heavy CPU usage)

--- i3+polybar/.config/i3/sakura.sh
+++ i3+polybar/.config/i3/sakura.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-pkill -f oneko
-nice -n 19 oneko -sakura -fg pink -bg '#222'
+
+echo === sakura ===
+while [ $(pidof oneko | wc -w) != 0 ]; do
+    echo An instance of oneko found, kill with SIGSYS !!
+    pkill --signal 31 -f oneko;
+    sleep 2
+done
+while [ $(pidof oneko | wc -w) != 1 ]; do
+    echo Invoking sakura !
+    nice -n 19 oneko -sakura -fg pink -bg '#222' -position +9-6
+    sleep 2
+done
+echo done !
+echo ===   End  ===
