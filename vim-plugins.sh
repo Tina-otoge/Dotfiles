56e62d179af53e29dde4c12be5c30e70a59977e4
Damien Savatier <damien.savatier@epitech.eu>
2017-10-17 08:49:43 +0200

    script to install vim plugins

---
+++ vim-plugins.sh
@@ -0,0 +1,10 @@
+c() {
+    git clone $1 ~/.vim/bundle/$2
+}
+
+
+c https://github.com/Valloric/YouCompleteMe YouCompleteMe
+c https://github.com/editorconfig/editorconfig-vim editorconfig-vim
+c https://github.com/scrooloose/nerdtree NERDTree
+c https://github.com/tpope/vim-fugitive vim-fugitive
+c https://github.com/vivien/vim-linux-coding-style LinuxCodingStyle
