33bd6973f646edeabc664da6c0819b41a6b2f1ff
Tina <skielred@gmail.com>
2019-06-11 17:39:05 +0200

    Revamps ycm_extra_conf, true support for C/C++

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -1,75 +1,66 @@
 import os
 import ycm_core
 
-flags = [
-    '-Wall',
-    '-Wextra',
-    '-Werror',
-    '-std=gnu11',
-    '-x', 'c',
-    # This path will only work on OS X, but extra paths that don't exist are not
-    # harmful
-    '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
-    '-isystem', '/usr/local/include',
-    '-isystem', '/usr/local/include/eigen3',
-    '-I', 'include'
-    '-I.'
-]
+def _get_file_extension(path):
+    return os.path.splitext(path)[1]
 
-compilation_database_folder = ''
+def _get_source_type(path):
+    extension = _get_file_extension(path)
+    types = {
+        '.c': 'c',
+        '.h': 'c',
+        '.cpp': 'c++',
+        '.hpp': 'c++',
+    }
 
-if compilation_database_folder:
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
-else:
-  database = None
+    return types.get(extension)
 
-
-def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
-
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return list( flags )
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
-
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
-
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
-
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
-
-
-def FlagsForFile( filename ):
-  if database:
-    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object
-    compilation_info = database.GetCompilationInfoForFile( filename )
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ )
-  else:
-    relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
-
-  return {
-    'flags': final_flags,
-    'do_cache': True
-  }
+def Settings(**kwargs):
+    language = kwargs['language']
+    file_path = kwargs.get('filename')
+    settings = {
+        'cfamily': {
+            'flags': [
+                '-Wall',
+                '-Wextra',
+                '-Werror',
+                '-Wconversion',
+                '-Wdouble-promotion',
+                '-Wduplicated-branches',
+                '-Wduplicated-cond',
+                '-Wjump-misses-init',
+                '-Wno-long-long',
+                '-Wnull-dereference',
+                '-Wshadow',
+                '-Wuseless-cast',
+                '-Iinclude',
+                '-I.',
+                # This path will only work on OS X, but extra paths that don't
+                # exist are not harmful
+                '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
+                '-isystem', '/usr/local/include',
+                '-isystem', '/usr/local/include/eigen3',
+            ]
+        }
+    }
+    result = settings.get(language)
+    extra_cfamily_settings = {
+        'c': {
+            'flags': [
+                '-x', 'c',
+                '-std=gnu11',
+            ]
+        },
+        'c++': {
+            'flags': [
+                '-x', 'c++',
+                'std=c++11',
+            ]
+        }
+    }
+    if language == 'cfamily':
+        actual_language = _get_source_type(file_path)
+        extra_settings = extra_cfamily_settings[actual_language]
+        for key in extra_settings:
+            result[key] += extra_settings[key]
+    return result
