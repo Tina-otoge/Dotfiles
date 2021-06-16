a7a4070e1bc84afbf5907e80b30cf552d2467920
Tina <me@tina.moe>
2021-06-16 10:58:20 +0200

    Initial install script

---
+++ install_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+link() {
+	[ -L "$2" ] && return 0
+	ln -sfbv --suffix=.backup "$1" "$2"
+}
+
+git submodules update --init
+
+mkdir -p ~/.config
+for file in ./*/ ./compton.conf ./picom.conf; do
+	link "$file" ~/.config
+done
+(
+	cd
+	link .config/vim .vim
+	link .config/bash/bashrc .bashrc
+	link .config/zsh/zshrc .zshrc
+)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+pip install --user thefuck
