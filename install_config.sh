214d25ddbb5243c3b4e53b53560f835bde6fcb80
Damien 🌱 <dsavatier@steamulo.com>
2023-01-11 18:03:57 +0100

    Update install script

--- install_config.sh
+++ install_config.sh
@@ -5,11 +5,11 @@ link() {
 	ln -sfbv --suffix=.backup "$1" "$2"
 }
 
-git submodules update --init
+git submodule update --init
 
 mkdir -p ~/.config
-for file in ./*/ ./compton.conf ./picom.conf; do
-	link "$file" ~/.config
+for file in ./*/ ./*.conf; do
+	link "$(realpath "$file")" ~/.config
 done
 (
 	cd
@@ -17,8 +17,8 @@ done
 	link .config/bash/bashrc .bashrc
 	link .config/zsh/zshrc .zshrc
 )
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-pip install --user thefuck
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# pip install --user thefuck
 gsettings set org.gnome.shell.app-switcher current-workspace-only false
 gsettings set org.gnome.shell.window-switcher current-workspace-only false
 echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
